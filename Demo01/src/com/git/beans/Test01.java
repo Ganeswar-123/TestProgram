0e7086bcf59515549833011e507fa2cca02946f2
Gm Sir <TL@gmail.com>
2023-04-16 10:06:21 +0530

    NO

---
+++ Demo01/src/com/git/beans/Test01.java
@@ -0,0 +1,5 @@
+package com.git.beans;
+
+public class Test01 {
+	public int x = 10;
+}
--- Demo01/src/com/git/commons/HellowWorld.java
+++ Demo01/src/com/git/commons/HellowWorld.java
@@ -1,9 +1,13 @@
 package com.git.commons;
 
+import com.git.beans.Test01;
+
 public class HellowWorld {
 
 	public static void main(String[] args) {
 		System.out.println("hello:: how are you ?");
+		Test01 t1 = new Test01();
+		System.out.println(t1.x);
 	}
 
 }
